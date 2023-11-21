f00ed09d8688d9c13111284c5554d0b3b9acb9fb
Michał Ziniewicz <michal.ziniewicz22@gmail.com>
2023-11-21 11:14:14 +0100

    Initial commit

---
+++ smobilne6UITests/smobilne6UITestsLaunchTests.m
@@ -0,0 +1,37 @@
+//
+//  smobilne6UITestsLaunchTests.m
+//  smobilne6UITests
+//
+//  Created by Patrycja Zadziłko on 16/11/2023.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface smobilne6UITestsLaunchTests : XCTestCase
+
+@end
+
+@implementation smobilne6UITestsLaunchTests
+
++ (BOOL)runsForEachTargetApplicationUIConfiguration {
+    return YES;
+}
+
+- (void)setUp {
+    self.continueAfterFailure = NO;
+}
+
+- (void)testLaunch {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    // Insert steps here to perform after app launch but before taking a screenshot,
+    // such as logging into a test account or navigating somewhere in the app
+
+    XCTAttachment *attachment = [XCTAttachment attachmentWithScreenshot:XCUIScreen.mainScreen.screenshot];
+    attachment.name = @"Launch Screen";
+    attachment.lifetime = XCTAttachmentLifetimeKeepAlways;
+    [self addAttachment:attachment];
+}
+
+@end
